4341aae4bb59e5f56c15223e89bbc276c119ab33
ikok07 <kokmarok@gmail.com>
2023-08-29 14:11:38 +0300

    add sound controller

---
+++ Sources/HorizontalNumberPicker/SoundController.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kaloyan Petkov on 29.08.23.
+//
+
+import Foundation
+import AVFoundation
+
+struct Sound {
+    
+    static func play() {
+        let url = Bundle.main.url(forResource: "click", withExtension: "wav")!
+        
+        do {
+            let player = try AVAudioPlayer(contentsOf: url)
+            player.play()
+        } catch {
+            print("Error playing audio: \(error)")
+        }
+    }
+    
+}
