922053dd6dd943c9b7fcbf1efb1a95e78c8023fb
ikok07 <kokmarok@gmail.com>
2023-08-31 12:49:09 +0300

    remove corner radius of triangle

--- Sources/HorizontalNumberPicker/TriangleShape.swift
+++ Sources/HorizontalNumberPicker/TriangleShape.swift
@@ -12,18 +12,11 @@ struct Triangle: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        
-        path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.midY),
-                          control: CGPoint(x: rect.minX, y: rect.minY))
-        
-        path.addQuadCurve(to: CGPoint(x: rect.midX, y: rect.minY),
-                          control: CGPoint(x: rect.minX, y: rect.minY))
-        
-        path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.midY),
-                          control: CGPoint(x: rect.maxX, y: rect.minY))
-        
-        path.closeSubpath()
+        path.move(to: CGPoint(x: rect.midX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.midY))
         
         return path
     }
